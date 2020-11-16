b5e6c28064242cf2a44ef7c784387b526584524e
AbhiKapoor55 <abhishek.kapoor@mail.utoronto.ca>
2020-11-15 21:48:59 -0500

    added HomePage, modified css file

---
+++ learn-aws/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+const HomePage = () => {
+    return (
+        <React.Fragment>
+            <h1>Welcome to LearnAWS.com</h1>
+            <p>Loren Ipsum Text Here</p>
+            <p>Loren Ipsum Text Here</p>
+            <p>Loren Ipsum Text Here</p>
+            <p>Loren Ipsum Text Here</p>
+        </React.Fragment>
+    )
+}
+
+export default HomePage; 
