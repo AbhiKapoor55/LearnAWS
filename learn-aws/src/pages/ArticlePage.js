f7d0ae44f0540c5a2282ca6a6f063b13f128fa6b
AbhiKapoor55 <abhishek.kapoor@mail.utoronto.ca>
2020-11-16 19:25:30 -0500

    added url parameters, added sample article content

--- learn-aws/src/pages/ArticlePage.js
+++ learn-aws/src/pages/ArticlePage.js
@@ -1,9 +1,19 @@
 import React from 'react'; 
+import articles from './article-content';
 
-const ArticlePage = () => {
+const ArticlePage = ({ match }) => {
+    const name = match.params.name; 
+    const article = articles.find(article => article.name === name); 
+
+    if (!article) return <h1>Article Does Not Exist!</h1>
+
     return (
         <React.Fragment>
-            <h1>This is an article!</h1>
+            <h1>This is the article on {article.title} !</h1>
+            {article.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))} 
+    
         </React.Fragment>
     )
 }
