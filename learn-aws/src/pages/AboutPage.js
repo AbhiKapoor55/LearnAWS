63c5830726aba5d3ce98e78f75dde8021ec1d855
AbhiKapoor55 <abhishek.kapoor@mail.utoronto.ca>
2020-11-16 19:14:46 -0500

    added navbar, improved styling

---
+++ learn-aws/src/pages/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+const AboutPage = () => {
+    return (
+        <React.Fragment>
+            <h1>About Me</h1>
+            <p>Loren Ipsum Text</p>
+            <p>Loren Ipsum Text</p>
+            <p>Loren Ipsum Text</p>
+            <p>Loren Ipsum Text</p>
+        </React.Fragment>
+    )
+}
+
+export default AboutPage; 
